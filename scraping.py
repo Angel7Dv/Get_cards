1f3899b577fe316683f2e1f9d18243a926dece49
angel <angelriera1796@gmail.com>
2022-03-06 23:11:54 -0400

    add env

--- scraping.py
+++ scraping.py
@@ -1,53 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-"""librerias: bs4 - requests - lxml """
 
 # leer cualquier web
 website = "https://leagueoflegends.fandom.com/wiki/Pantheon/LoL/Audio"
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, 'lxml')
+
+
 tags = soup.select('i, b , audio')
+""" Extre las etiquetas que creeomes que tiene el contenido importante"""
 
+urls_text = re.compile(r'(?<=src=").*?(?=")|(?<=>").*?(?="<)') #
+""" extrae la url de las etiquetas y el texto de una etiqueta"""
 
-# EXTRER LOS ELEMENTOS DE UNA WEB
-""" se recomienda buscar elementos en este orden = id > classname > tagname cssSelector > Xpath"""
-
-# tags = soup.findAll(re.compile(r'(i)'))
-# tags = soup.find_all(['i', 'b', 'audio' ])
-# r = re.compile('(?<=src=").*?(?=")')
-# audio='src="(.*?)"', text= '>"([^<>]*)"'
-
-# content = open('cosa.txt', 'r')
-
-# for i in content:
-#     content = i
-
-
-
-
-urls_text = re.compile(r'(?<=src=").*?(?=")|(?<=>)".*?"(?=<)') #
-
-
-result = urls_text.findall(str(tags))
-print(result)
-
-
-# for line in tags:
-#     #text = line.get_text()
-#     string = str(line)
-#     url = urls_text.findall(string)
-#     print(url)
-
-
-#print( text, audio , " \n")
-#for i in soup:
-
-"obtener todas las etiquetas audio y i dentro de content"
-
-# audios = content.find_all('audio')
+resultado = urls_text.findall(str(tags))
+print(resultado)
 
 # CREAR UN ARCHIVO CON LA INFORMACION
 with open('phanteon.txt', 'w') as file: 
-    file.write(str(result))
+    file.write(str(resultado))
---
+++ todo.md
@@ -0,0 +1,5 @@
+
+- [x] Extraer datos de la pagina 
+- [ ] Crear una base de datos con los datos extraidos
+- [ ] Descargar los archivos de audio y alamcenarlos en rutas espesificas
+- [ ] Crear targetas con los archivos y el contenido de la base de datos
