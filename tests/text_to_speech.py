e1030d767b5ddee6c8534cda4604d5d42f69d47b
angel <angelriera1796@gmail.com>
2022-05-06 15:30:47 -0400

    .

--- tests/text_to_speech.py
+++ tests/text_to_speech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS
 import os
-mytext= """Presentarte como una persona profesional va mucho más allá de tu CV y tu perfil en LinkedIn. Ya sea si quieres tener un negocio propio, una agencia, quieres ser freelancer o quieres un mejor empleo, debes saber que tu presencia en línea lo es todo para alcanzar tus metas profesionales."""
+mytext= """Presentarte como una persona profesional"""
 language='es'
 myobj=gTTS(text=mytext,lang=language)
 myobj.save("test.mp3")
